e8c06f5c3831a49dcc5df29c9653169b39e4bf4e
Justin Chase <justin.m.chase@gmail.com>
2016-05-07 09:55:08 -0500

    Generate basic routes

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 var assert = require('assert')
 var coleslaw = require('coleslaw')
 
+// POST            Creates a new resource.
+// GET             Retrieves a resource.
+// PUT             Updates an existing resource.
+// DELETE          Deletes a resource.
+
 function generateCreateRoute(options, model) {
     var router = options.express.Router()
-    router.put('/', (req, res, next) => {
-        var data = req.body;
+    router.post('/', (req, res, next) => {
+        var data = req.body
         options.dataAccess[model.name].create(data, (err, result) => {
-            if (err) return next(err);
-            res.status(201).send(result)
+            if (err) return next(err)
+            res.status(200).send(result)
         })
     })
     return router
@@ -15,25 +20,50 @@ function generateCreateRoute(options, model) {
 
 function generateRetrieveRoute(options, model) {
     var router = options.express.Router()
+    model.fields.forEach(field => {
+        if (field.index) {
+            router.get('/:' + field.name, (req, res, next) => {
+                var data = req.params
+                options.dataAccess[model.name].create(data, (err, result) => {
+                    if (err) return next(err)
+                    res.status(200).send(result)
+                })
+            })
+        }
+    })
     return router
 }
 
 function generateUpdateRoute(options, model) {
     var router = options.express.Router()
+    router.put('/', (req, res, next) => {
+        var data = req.body
+        options.dataAccess[model.name].create(data, (err, result) => {
+            if (err) return next(err)
+            res.status(200).send(result)
+        })
+    })
     return router
 }
 
 function generateDeleteRoute(options, model) {
     var router = options.express.Router()
+    router.delete('/', (req, res, next) => {
+        var data = req.body
+        options.dataAccess[model.name].delete(data, (err, result) => {
+            if (err) return next(err)
+            res.status(200).send({ ok: true })
+        })
+    })
     return router
 }
 
 function generateCrudRoutes(options, model) {
     var router = options.express.Router()
-    router.use(`/create`, generateCreateRoute(options, model))
-    router.use(`/retrieve`, generateRetrieveRoute(options, model))
-    router.use(`/update`, generateUpdateRoute(options, model))
-    router.use(`/delete`, generateDeleteRoute(options, model))
+    router.use(generateCreateRoute(options, model))
+    router.use(generateRetrieveRoute(options, model))
+    router.use(generateUpdateRoute(options, model))
+    router.use(generateDeleteRoute(options, model))
     return router
 }
 
@@ -42,7 +72,6 @@ function generate(options, model) {
     assert(model)
 
     if (!options.express) options.express = require('express')
-
     if (typeof model === 'object' && model.length) {
         return model
             .map(m => generate(express, m))
